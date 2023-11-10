288c303a9cea3552012b1b7a5ff22bc7d7b5cc29
Eusuf Uddin <eusuf15-8926@diu.edu.bd>
2023-11-10 19:54:45 +0600

    minor changes

--- lib/onboardscreen.dart
+++ lib/onboardscreen.dart
@@ -21,32 +21,40 @@ class _OnBoardScreenState extends State<OnBoardScreen> {
             body: "This is First Title",
             image: Image.network(
               "https://e7.pngegg.com/pngimages/735/657/png-clipart-onboarding-user-experience-workflow-management-computer-service-office.png",
-              width: 350,)
-        ),
+              width: 350,
+            )),
         PageViewModel(
             title: "Second Title",
             body: "This is Second Title",
             image: Image.network(
               "https://w7.pngwing.com/pngs/771/31/png-transparent-employee-engagement-employee-retention-human-resource-onboarding-employee-engagement-text-logo-sign-thumbnail.png",
-              width: 350,)
-        ),
+              width: 350,
+            )),
         PageViewModel(
             title: "Third Title",
             body: "This is Third Title",
             image: Image.network(
               "https://e7.pngegg.com/pngimages/735/657/png-clipart-onboarding-user-experience-workflow-management-computer-service-office.png",
-              width: 350,)
-        )
+              width: 350,
+            ))
       ],
-      onDone:(){
-        Navigator.push(context, CupertinoPageRoute(builder: (context)=> MyApp()));
+      onDone: () {
+        Navigator.push(
+            context, CupertinoPageRoute(builder: (context) => MyApp()));
       },
       done: Text("Done"),
       skip: Text("Skip"),
       showSkipButton: false,
       next: Text("Next"),
       showNextButton: true,
-
+      dotsDecorator: const DotsDecorator(
+          color: Colors.blue,
+          activeColor: Colors.black,
+          activeSize: Size(20, 10),
+          activeShape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20.0)),
+          ),
+          size: Size(10, 10)),
     );
   }
 }
