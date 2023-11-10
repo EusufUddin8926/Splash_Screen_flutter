ce65a3be86037dbfb1d6c7c76de64e047c05530c
Eusuf Uddin <eusuf15-8926@diu.edu.bd>
2023-11-10 12:40:09 +0600

    Add Timer to wait 3s

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   void initState() {
-    Timer(Duration(seconds: 3), () {
+    Timer(const Duration(seconds: 3), () {
       Navigator.push(context as BuildContext, CupertinoPageRoute(builder: (context)=> HomePage()));
     });
     super.initState();
