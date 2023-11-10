d4b1611206a2682fef4d83fceda950388c707d47
Eusuf Uddin <eusuf15-8926@diu.edu.bd>
2023-11-10 16:49:00 +0600

    Add Homepage with CarouselSlider

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,9 +1,33 @@
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_carousel_slider/carousel_slider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<Color> colors = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.blue,
+    Colors.indigo,
+    Colors.purple,
+  ];
+
+  final List<String> letters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +36,25 @@ class HomePage extends StatelessWidget {
         title: Text("Home Page"),
         backgroundColor: Colors.blue,
         elevation: 5.0,
+      ),
+      body: SizedBox(
+        height: 400,
+        child: CarouselSlider.builder(
+            slideBuilder: (index) {
+              return Container(
+                alignment: Alignment.center,
+                color: colors[index],
+                child: Text(
+                  letters[index],
+                  style: TextStyle(fontSize: 200, color: Colors.white),
+                ),
+              );
+            },
+            slideTransform: const CubeTransform(),
+            slideIndicator: CircularSlideIndicator(
+              padding: const EdgeInsets.only(bottom: 32),
+            ),
+            itemCount: colors.length),
       ),
     );
   }
